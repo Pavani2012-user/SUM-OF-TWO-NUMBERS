9b833d9bf823ab7748f4c1753bc42a92bd1a24b3
Pavani2012-user <pavanibattu006@gmail.com>
2025-01-25 15:54:56 +0530

    Add files via upload

---
+++ SumOfTwoNumbers.java
@@ -0,0 +1,20 @@
+public class SumOfTwoNumbers {
+        public static void main(String[] args) {
+            // Example numbers
+            int num1 = 5;
+            int num2 = 7;
+    
+            // Call the function to get the sum
+            int result = addNumbers(num1, num2);
+    
+            // Print the result
+            System.out.println("The sum of " + num1 + " and " + num2 + " is: " + result);
+        }
+    
+        // Function to add two numbers and return the sum
+        public static int addNumbers(int a, int b) {
+            return a+b;
+        }
+    }
+    
+
